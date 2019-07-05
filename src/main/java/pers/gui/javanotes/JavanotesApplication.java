1d74fe8fdfe26fa6898ce32f9de2aadf1427d776
guiaixiao <824541050@qq.com>
2019-07-05 16:48:25 +0800

    init project

---
+++ src/main/java/pers/gui/javanotes/JavanotesApplication.java
@@ -0,0 +1,13 @@
+package pers.gui.javanotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavanotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/gui/javanotes/JavanotesApplicationTests.java
@@ -0,0 +1,16 @@
+package pers.gui.javanotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavanotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
